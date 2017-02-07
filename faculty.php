fcc8c9301c652d6d57595b5012be40b20341cd44
Neil Angelo <naa.pereyra@iskolarngbayan.pup.edu.ph>
2017-02-07 11:04:23 +0800

    Added faculty tab

--- faculty.php
+++ faculty.php
@@ -12,40 +12,40 @@
 	<?PHP include "header.php"; ?>
 
 	<div class="mod-bg" id="new-stu-mod">
-		<div class="form mod" id="reg-student">
+		<div class="form mod" id="reg-faculty">
 			<form action="">
 				<div class="formheader">
-					<h2>Register Student</h2>
+					<h2>Register Faculty</h2>
 				</div>
 				<div class="subform">
 					<h3 class="subheader">Personal Information</h3>
 					<div class="align">
-						<label for="stud_id">Student ID: </label>
-						<input type="text" name= "stud_id" id="stud_id" required>
+						<label for="f_id">Faculty ID: </label>
+						<input type="text" name= "f_id" id="f_id" required>
 					</div>
 					<div class="align">
-						<label for="stud_fname">First Name: </label>
-						<input type="text" name= "stud_fname" id="stud_fname" required>
+						<label for="f_fname">First Name: </label>
+						<input type="text" name= "f_fname" id="f_fname" required>
 					</div>
 					<div class="align">
-						<label for="stud_lname">Last Name: </label>
-						<input type="text" name= "stud_lname" id="stud_lname" required>
+						<label for="f_lname">Last Name: </label>
+						<input type="text" name= "f_lname" id="f_lname" required>
 					</div>
 					<div class="align">
-						<label for="stud_mname">Middle Name: </label>
-						<input type="text" name= "stud_mname" id="stud_mname">
+						<label for="f_mname">Middle Name: </label>
+						<input type="text" name= "f_mname" id="f_mname">
 					</div>
 					<div class="align">
-						<label for="stud_age">Age: </label>
-						<input type="number" name= "stud_age" id="stud_age" required>
+						<label for="f_age">Age: </label>
+						<input type="number" name= "f_age" id="f_age" required>
 					</div>
 					<div class="align">
-						<label for="stud_dob">Birthday: </label>
-						<input type="date" name= "stud_dob" id="stud_dob" required>
+						<label for="f_dob">Birthday: </label>
+						<input type="date" name= "f_dob" id="f_dob" required>
 					</div>
 					<div class="align">
-						<label for="stud_gender">Gender</label>
-						<select name="stud_gender" id="stud_gender">
+						<label for="f_gender">Gender</label>
+						<select name="f_gender" id="f_gender">
 							<option value="M">Male</option>
 							<option value="F">Female</option>
 						</select>
@@ -54,20 +54,20 @@
 				<div class="subform">
 					<h3 class="subheader">Contact</h3>
 					<div class="align">
-						<label for="stud_stadd">Street: </label>
-						<input type="text" name= "stud_stadd" id="stud_stadd" required>
+						<label for="f_stadd">Street: </label>
+						<input type="text" name= "f_stadd" id="f_stadd" required>
 					</div>
 					<div class="align">
-						<label for="stud_ctadd">City: </label>
-						<input type="text" name= "stud_ctadd" id="stud_ctadd" required>
+						<label for="f_ctadd">City: </label>
+						<input type="text" name= "f_ctadd" id="f_ctadd" required>
 					</div>
 					<div class="align">
-						<label for="stud_email_add">Email: </label>
-						<input type="email" name= "stud_email_add" id="stud_email_add" required>
+						<label for="f_email">Email: </label>
+						<input type="email" name= "f_email" id="f_email" required>
 					</div>
 					<div class="align">
-						<label for="stud_mobno">Mobile Number: </label>
-						<input type="number" name= "stud_mobno" id="stud_mobno" required>
+						<label for="dept_id">Department Code: </label>
+						<input type="text" name= "dept_id" id="dept_id" required>
 					</div>
 				</div>
 				<div class="buttons">
@@ -80,9 +80,9 @@
 
 	<div class="tbl v">
 		<div class="tbl-new-b-c">
-			<button type="button" class="btn b-s new-b-s" id="new-stu-btn" onclick="document.getElementById('new-stu-mod').style.display = 'block' ">Register Student</button>
+			<button type="button" class="btn b-s new-b-s" id="new-stu-btn" onclick="document.getElementById('new-stu-mod').style.display = 'block' ">Register Faculty</button>
 		</div>
-		<h2>Students</h2>
+		<h2>Faculties</h2>
 		
 		<table>
 			<tr>
